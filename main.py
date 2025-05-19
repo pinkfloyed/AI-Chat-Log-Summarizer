4c271ed088edfcbb0c9e52001d826c9c6479a4d8
pinkfloyed <pinkiakter195@gmail.com>
2025-05-19 11:14:15 +0600

    Updated main.py file

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from chat_parser import parse_chat
+from utils import extract_keywords
+from summary_generator import generate_summary
+
 
 def main():
     chat_file = "data/chat.txt"
     user_msgs, ai_msgs = parse_chat(chat_file)
+    generate_summary(user_msgs, ai_msgs, top_n=10)
 
 if __name__ == "__main__":
     main()
